8a72fc9d8c181ec5da16e9ad7a8d541a5b201371
omotcha <omotcha2097@gmail.com>
2023-03-28 10:13:32 +0800

    provider-consumer arch

---
+++ provider-service/src/main/java/org/omotcha/controllers/DeptController.java
@@ -0,0 +1,19 @@
+package org.omotcha.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/dept/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "<h2>Success</h2>service name：spring-cloud-alibaba-provider<br /> service port： " +
+                serverPort + "<br /> param：" + id;
+    }
+
+}
