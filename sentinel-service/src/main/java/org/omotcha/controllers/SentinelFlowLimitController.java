9c19d3f8cd5f46a5f610873efcc701b5582a1f9e
omotcha <omotcha2097@gmail.com>
2023-03-28 16:01:24 +0800

    flow control

--- sentinel-service/src/main/java/org/omotcha/controllers/SentinelFlowLimitController.java
+++ sentinel-service/src/main/java/org/omotcha/controllers/SentinelFlowLimitController.java
@@ -5,10 +5,16 @@ import com.alibaba.csp.sentinel.SphO;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -33,6 +39,14 @@ public class SentinelFlowLimitController {
         log.info("test C success: "+serverPort);
         return "test C success: "+serverPort;
         //business logic end
+    }
+
+    @GetMapping("/testD")
+    @SentinelResource(value = "testD-resource", blockHandler = "blockHandlerTestD")
+    public String testD() {
+        initFlowRules();
+        log.info("test D success: "+serverPort);
+        return "test D success: "+serverPort;
     }
 
     // defining sentinel resource by SphU
@@ -75,5 +89,25 @@ public class SentinelFlowLimitController {
             return "test B been limited: "+serverPort;
             //dataflow control logic ends
         }
+    }
+
+    public String blockHandlerTestD(BlockException exception) {
+        log.info(Thread.currentThread().getName() + "TestD access denied! Please try it later.");
+        return "TestD access denied! Please try it later.";
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        //定义一个限流规则对象
+        FlowRule rule = new FlowRule();
+        //资源名称
+        rule.setResource("testD-resource");
+        //限流阈值的类型
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // 设置 QPS 的阈值为 2
+        rule.setCount(2);
+        rules.add(rule);
+        //定义限流规则
+        FlowRuleManager.loadRules(rules);
     }
 }
